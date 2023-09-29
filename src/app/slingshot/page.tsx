7b34b1260977ace3f6ea3c038a1e0003ce1f2f69
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-29 10:52:02 +0900

    Chore : yarn add matter-js

--- src/app/slingshot/page.tsx
+++ src/app/slingshot/page.tsx
@@ -1,3 +1,9 @@
+import SlingShot from "@/components/SlingShot/SlingShot";
+
 export default function SlingShotPage() {
-  return <div className="flex-col-full flex-center">SlingShotPage</div>;
+  return (
+    <div className="flex-col-full flex-center">
+      <SlingShot />
+    </div>
+  );
 }

4c56560be593b58cd573e0ebc85b204690679d88
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-10-03 20:06:06 +0900

    Feat : add link to footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { PropsWithChildren } from "react";
 import { useRouter } from "next/navigation";
-import { Text } from "@chakra-ui/react";
+import { Divider, StepSeparator, Text } from "@chakra-ui/react";
 import ContentArea from "./ContentArea";
+import Link from "next/link";
 
 interface Props extends PropsWithChildren {
   className?: string;
@@ -10,9 +11,18 @@ interface Props extends PropsWithChildren {
 
 const Footer = ({ children }: Props) => {
   return (
-    <footer className={"w-full min-h-[3rem] pt-2 bg-[#222222] text-[#eeeeee]"}>
+    <footer className={"w-full min-h-[3rem] pt-2 bg-[#222222] text-[#dddddd]"}>
       <ContentArea>
         {children}
+
+        <div className="flex gap-3 h-4 my-2 items-center justify-center">
+          <Text>개발자 문의: dusunax@gmail.com</Text>
+          <Divider orientation="vertical" />
+          <Link href={"https://github.com/dusunax/game"}>
+            코드 저장소:{" "}
+            <span className="underline">https://github.com/dusunax/game</span>
+          </Link>
+        </div>
 
         <Copyright />
       </ContentArea>
